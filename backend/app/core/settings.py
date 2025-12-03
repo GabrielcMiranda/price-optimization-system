84355e1358f7197d319dcce0241e5e08a679d9e6
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-12-03 01:13:37 -0300

    feat: adicionando configuracoes de seguranca

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -5,3 +5,6 @@ class Settings:
 
     load_dotenv()
     PG_URL = getenv('PG_URL')
+    SECRET_KEY = getenv('SECRET_KEY')
+    ALGORITHM = getenv('ALGORITHM')
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 600))   
