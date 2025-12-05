a955fdb63a0c3f057c848eb3119517ebe404b9fd
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-12-05 01:59:22 -0300

    feat: adicionando tela de otimizacao

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.auth_router import auth_router
 import uvicorn
 from app.routers.optimization_router import optimization_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 app.include_router(auth_router)
 app.include_router(optimization_router)
