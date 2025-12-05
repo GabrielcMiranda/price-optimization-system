78bdd7ec2aad36228116e47eca0c393c4150cdad
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-12-04 23:13:20 -0300

    feat: adicionando guards e interceptors ao projeto

---
+++ frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -0,0 +1,37 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  
+  const token = authService.getToken();
+  
+  if (token && authService.isTokenValid()) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  } else if (token && !authService.isTokenValid()) {
+    router.navigate(['/auth/login']);
+  }
+  
+  return next(req).pipe(
+    catchError((error) => {
+  
+      if (error.status === 401) {
+        console.warn('Erro 401: Token rejeitado pelo servidor. Redirecionando para login...');
+        
+        authService.removeToken();
+        
+        router.navigate(['/auth/login']);
+      }
+      
+      return throwError(() => error);
+    })
+  );
+};
